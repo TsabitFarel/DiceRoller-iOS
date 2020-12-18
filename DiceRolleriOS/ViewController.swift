c16c3a26bced32c1c833a349746e51550c1b7491
tsabitfarel <tsabitzfarel@gmail.com>
2020-12-18 19:55:12 +0800

    Initial Commit

---
+++ DiceRolleriOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiceRolleriOS
+//
+//  Created by Tsabit Farel on 18/12/20.
+//  Copyright © 2020 Tsabit Farel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
