cb5de4b4142b6c62ac43f53f2e2b9c58d64a52bc
TsabitFarel <tsabitzfarel@gmail.com>
2020-12-18 20:41:54 +0800

    init

--- DiceRolleriOS/ViewController.swift
+++ DiceRolleriOS/ViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func buttonRollDice(_ sender: Any) {
+        let diceImages = [ #imageLiteral(resourceName: "diceone"), #imageLiteral(resourceName: "dicetwo"), #imageLiteral(resourceName: "dicethree"), #imageLiteral(resourceName: "dicefour"), #imageLiteral(resourceName: "dicefive"), #imageLiteral(resourceName: "dicesix") ]
+        
+        diceImageView1.image = diceImages[Int.random(in: 0...5)]
+        
+        diceImageView2.image = diceImages[Int.random(in: 0...5)]
+    }
+    
 }
 
